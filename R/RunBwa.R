51762a73f0a66b9dce2ea762a3f3626f6929bee0
Zhang <zhangz@email.chop.edu>
2018-10-15 23:55:40 -0400

    run bwa

--- R/RunBwa.R
+++ R/RunBwa.R
@@ -25,7 +25,7 @@ RunBwaMemSingle <- function(fq, ref, bwa, samtools, output='', min.length=30, se
 }
 
 #######################################################
-RunBwaMemPair <- function(fq1, fq2, ref, bwa, samtools, output, min.length=30, seed.length=19, threads=1, all=FALSE) {
+RunBwaMemPair <- function(fq1, fq2, ref, bwa, samtools, output, min.length=30, seed.length=19, threads=1, all=FALSE, sam=TRUE) {
 
   f1 <- paste(output, '.sam', sep='');
   f2 <- sub('.sam$', '.bam', f1);
@@ -33,9 +33,14 @@ RunBwaMemPair <- function(fq1, fq2, ref, bwa, samtools, output, min.length=30, s
 
   cmd1 <- paste(bwa, 'mem', '-k', seed.length, '-T', min.length, '-t', threads);
   if (all) cmd1 <- paste(cmd1, '-a');
-  cmd1 <- paste(cmd1, ref, fq1, fq2, '>', f1);
 
-  cmd2 <- paste(samtools, 'view -Sb', f1, '>', f2);
+  if (sam) {
+    cmd1 <- paste(cmd1, ref, fq1, fq2, '>', f1);
+    cmd2 <- paste(samtools, 'view -Sb', f1, '>', f2);
+  } else {
+    cmd1 <- paste(cmd1, ref, fq, '|', samtools, 'view -Sb >', f2);
+    cmd2 <- '';
+  }
   cmd3 <- paste(samtools, 'sort', f2, '-o', f3);
   cmd4 <- paste(samtools, 'index', f3);
 
